25f5a4f71b350e327be74e5374cee78881db6d3d
Olga <olga.repnitsyna@gmail.com>
2019-01-15 09:56:03 -0500

    html and some css, added questions-answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,50 @@
+var questionAnswer = [{
+    question: "What is the largest country in the world?",
+    answer: ["Canada", "China", "Russia", "USA"],
+    correctAnswer: 2
+    }, {
+    question: "What has the most gramms of vitamin C in 100 gramms?",
+    answer: ["Kiwi", "Orange", "Broccoli", "Strawberries"],
+    correctAnswer: 0
+    }, {
+    question: "What is the biggest planet in solar system?",
+    answer: ["Saturn", "Jupiter", "Earth", "Uranus"],
+    correctAnswer: 1
+    }, {
+    question: "How many square inches in 1 square meter?",
+    answer: ["2325", "1550", "2015", "1240"],
+    correctAnswer: 1
+    }, {
+    question: "What is the deepest lake in the world?",
+    answer: ["Tahoe", "Malawi", "Caspian Sea", "Baikal"],
+    correctAnswer: 3
+    }, {
+    question: "What is the largest city by population in the world?",
+    answer: ["Tokyo - Japan", "New York - USA", "Shanghai - China", "Delhi - India"],
+    correctAnswer: 0
+    }, {
+    question: "Which from the list is not a musical band?",
+    answer: ["Modern Talking", "Roxette", "Vitas", "Little Big"],
+    correctAnswer: 2
+    }, {
+    question: "How many bone are in the human body?",
+    answer: ["216", "208", "206", "198"],
+    correctAnswer: 2
+    }, {
+    question: "Which from the list is not an island?",
+    answer: ["Santorini", "Maui", "Tasmania", "Tuvalu"],
+    correctAnswer: 3
+    }, {
+    question: "What is the biggest star?",
+    answer: ["Sirius", "Canopus", "Alpha", "Centauri"],
+    correctAnswer: 0
+    }];
+
+
+  console.log(questionAnswer); 
+     
+    
+  
+    
+
+      
